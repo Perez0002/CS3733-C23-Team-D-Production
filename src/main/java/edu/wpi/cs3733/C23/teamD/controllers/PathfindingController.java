3341b5afaeb20ba7221bc3fc8352206ebb1bdace
mtwilkinson <mtwilkinson@wpi.edu>
2023-01-29 14:46:59 -0500

    started adding pathfinding UI

--- src/main/java/edu/wpi/cs3733/C23/teamD/controllers/PathfindingController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/controllers/PathfindingController.java
@@ -5,14 +5,11 @@ import edu.wpi.cs3733.C23.teamD.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.fxml.FXML;
-import org.w3c.dom.Text;
 
 public class PathfindingController {
   @FXML private MFXButton cancelButton;
 
   @FXML private MFXTextField endRoom;
-
-  @FXML private Text pathText;
 
   @FXML private MFXTextField startRoom;
 
