1f71849b3524dce9a303ef9ef98dd0aed3528928
wyatt522 <75394799+wyatt522@users.noreply.github.com>
2023-01-29 17:05:44 -0500

    added menu bar

--- src/main/java/edu/wpi/cs3733/C23/teamD/controllers/RootController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/controllers/RootController.java
@@ -1,8 +1,25 @@
 package edu.wpi.cs3733.C23.teamD.controllers;
 
+import edu.wpi.cs3733.C23.teamD.navigation.Navigation;
+import edu.wpi.cs3733.C23.teamD.navigation.Screen;
 import javafx.fxml.FXML;
 
 public class RootController {
   @FXML
   public void initialize() {}
+
+  @FXML
+  void openHomepage() {
+    Navigation.navigate(Screen.HOME);
+  }
+
+  @FXML
+  void openPathfindingForm() {
+    Navigation.navigate(Screen.PATHFINDING_REQUEST);
+  }
+
+  @FXML
+  void openSanitationForm() {
+    Navigation.navigate(Screen.SANITATION_FORM);
+  }
 }
