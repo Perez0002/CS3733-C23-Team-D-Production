c965e1fcd3d855695852c721bac31562bf8b53fb
wyatt522 <75394799+wyatt522@users.noreply.github.com>
2023-02-05 16:29:21 -0500

    added submit button template functionality to the login page, yet to implement database or encryption as pects

--- src/main/java/edu/wpi/cs3733/C23/teamD/controllers/LoginController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/controllers/LoginController.java
@@ -1,5 +1,6 @@
 package edu.wpi.cs3733.C23.teamD.controllers;
 
+import edu.wpi.cs3733.C23.teamD.entities.LoginData;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
@@ -7,8 +8,9 @@ import javafx.scene.text.Text;
 public class LoginController {
 
   private boolean helpVisible = false;
-  private boolean incorrectInfoVisible = false;
   @FXML private Text incorrectUsernameOrPasswordText;
+
+  @FXML private Text successfulLoginText;
 
   @FXML private Text usernameText;
 
@@ -45,4 +47,88 @@ public class LoginController {
     password.clear();
     // System.out.println("clearFields"); // for debugging purposes
   } // end clearFields()
+
+  /*
+  checkFields
+  @param void
+  @return boolean
+  helper function for submit() function,
+  ensures all necessary fields are filled before submission
+  */
+  private boolean checkFields() {
+    if (checkPassword() && checkUsername()) {
+      return true;
+    } else {
+      if (!helpVisible) {
+        displayHelp();
+      }
+      return false;
+    }
+  } // end checkFields()
+
+  /**
+   * checks to see if there is any text in the username field
+   *
+   * @return true if there is any text
+   */
+  private boolean checkUsername() {
+    return (!username.getText().equals(""));
+  }
+
+  /**
+   * checks to see if there is any text in the password field
+   *
+   * @return true if there is any text
+   */
+  private boolean checkPassword() {
+    return (!password.getText().equals(""));
+  }
+
+  @FXML
+  /*
+  submit()
+  @param void
+  @return void
+  linked to "submit" button on SceneBuilder page, when selected
+  submits information filled out in forms
+  */
+  public void submitLogin() {
+    if (checkFields()) {
+      LoginData loginInfo =
+          new LoginData(
+              username.getText(),
+              password.getText(),
+              LoginData.Status.DONE); // creates PatientTransportData object
+      if (validUserCheck(username.getText(), password.getText())) {
+        successfulLoginText.setVisible(true);
+        incorrectUsernameOrPasswordText.setVisible(false);
+      } else {
+        successfulLoginText.setVisible(false);
+        incorrectUsernameOrPasswordText.setVisible(true);
+      }
+
+      // loginInfo.printInformation(); // for debugging
+      //      Database Stuff?:
+      //      Connection conn = Ddb.makeConnection();
+      //      Ddb.insertNewForm(conn, patientInformation);
+      //      try {
+      //        conn.close();
+      //      } catch (SQLException e) {
+      //        e.printStackTrace();
+      //      }
+
+    }
+  } // end submit()
+
+  /**
+   * check's if the user is in the database
+   *
+   * @param username the username to check
+   * @param password the password to check
+   * @return true if the password and username match
+   */
+  public boolean validUserCheck(String username, String password) {
+    // for now just returns false since IDK how to do the encryption and database stuff
+    return false;
+  }
 }
--- src/main/java/edu/wpi/cs3733/C23/teamD/entities/LoginData.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/entities/LoginData.java
@@ -1,2 +1,26 @@
-package edu.wpi.cs3733.C23.teamD.entities;public class LoginData {
+package edu.wpi.cs3733.C23.teamD.entities;
+
+public class LoginData {
+  private String username;
+  private String password;
+
+  public enum Status {
+    BLANK,
+    PROCESSING,
+    DONE;
+  }
+
+  private Status stat;
+
+  public LoginData(String username, String password, Status stat) {
+    this.username = username;
+    this.password = password;
+    this.stat = stat;
+  }
+
+  public void printInformation() {
+    System.out.println("The patientID is " + this.username);
+    System.out.println("The endRoom " + this.password);
+    System.out.println("The equipment required is " + this.stat);
+  }
 }
