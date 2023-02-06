a65e02007818a9b2d61cfad1c9a185b3b0f159c6
DireChicken200 <35498545+DireChicken200@users.noreply.github.com>
2023-02-05 21:42:28 -0500

    Small changes to be closer to completion for editing nodes once database is up

--- src/main/java/edu/wpi/cs3733/C23/teamD/controllers/pathfinding/MapEditorPageController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/controllers/pathfinding/MapEditorPageController.java
@@ -3,6 +3,7 @@ package edu.wpi.cs3733.C23.teamD.controllers.pathfinding;
 import static edu.wpi.cs3733.C23.teamD.Ddb.createJavaNodes;
 import static edu.wpi.cs3733.C23.teamD.Ddb.makeConnection;
 
+import edu.wpi.cs3733.C23.teamD.entities.Edge;
 import edu.wpi.cs3733.C23.teamD.entities.Node;
 import edu.wpi.cs3733.C23.teamD.navigation.Navigation;
 import edu.wpi.cs3733.C23.teamD.navigation.Screen;
@@ -11,9 +12,12 @@ import java.sql.Connection;
 import java.util.ArrayList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Point2D;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
+import net.kurobako.gesturefx.GesturePane;
 
 public class MapEditorPageController {
 
@@ -53,7 +57,9 @@ public class MapEditorPageController {
   }
 
   @FXML
-  void submit() {}
+  void submit() {
+
+  }
 
   @FXML
   void displayHelp() {
@@ -68,7 +74,16 @@ public class MapEditorPageController {
     MapDrawController mapDrawer = new MapDrawController();
     Connection conn = makeConnection();
     ArrayList<Node> nodeList = createJavaNodes(conn);
-    mapEditorPane.setCenter(
+    double sumX = 0;
+    double sumY = 0;
+    double total = nodeList.size();
+    for (Node node : nodeList) {
+      sumX += node.getXcoord();
+      sumY += node.getYcoord();
+    }
+    double avgX = sumX / total;
+    double avgY = sumY / total;
+    GesturePane mainPane =
         mapDrawer.genMapFromNodes(
             nodeList,
             node -> {
@@ -77,8 +92,15 @@ public class MapEditorPageController {
                 public void handle(MouseEvent event) {
                   currentNodeEdit = node;
                   nodeInformationText.setText(currentNodeEdit.getNodeID());
+                  longNameTextField.setText("Can't get node name from Node");
+                  shortNameTextField.setText("Can't get node name from Node");
+                  roomTypeTextField.setText("Can't get node room type from Node");
                 }
               };
-            }));
+            });
+    mapEditorPane.setCenter(mainPane);
+    mainPane.zoomTo(mainPane.getMinScale(), new Point2D(avgX, avgY));
+    // TODO wwwwwwwhhhhhhhyyyyyyyy can't I get the size of the fluffing GesturePane ;-;
+    mainPane.animate(Duration.millis(1500)).centreOn(new Point2D(avgX - 750, avgY - 750));
   }
 }
