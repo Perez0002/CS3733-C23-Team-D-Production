ad036f9e9f8eae613d8169e9c81de82b1481428a
DireChicken200 <35498545+DireChicken200@users.noreply.github.com>
2023-02-06 16:10:49 -0500

    Added light changeable node functionality, still needs to be linked to DB

--- src/main/java/edu/wpi/cs3733/C23/teamD/controllers/pathfinding/MapEditorPageController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/controllers/pathfinding/MapEditorPageController.java
@@ -1,9 +1,10 @@
 package edu.wpi.cs3733.C23.teamD.controllers.pathfinding;
 
-import static edu.wpi.cs3733.C23.teamD.Ddb.createJavaNodes;
-import static edu.wpi.cs3733.C23.teamD.Ddb.makeConnection;
+import static edu.wpi.cs3733.C23.teamD.Ddb.*;
 
+import edu.wpi.cs3733.C23.teamD.entities.Edge;
 import edu.wpi.cs3733.C23.teamD.entities.Node;
+import edu.wpi.cs3733.C23.teamD.entities.locationName;
 import edu.wpi.cs3733.C23.teamD.navigation.Navigation;
 import edu.wpi.cs3733.C23.teamD.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXTextField;
@@ -12,8 +13,11 @@ import java.util.ArrayList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
+import net.kurobako.gesturefx.GesturePane;
 
 public class MapEditorPageController {
 
@@ -37,6 +41,11 @@ public class MapEditorPageController {
 
   private Node currentNodeEdit;
 
+  private ArrayList<Node> nodeList;
+  private ArrayList<locationName> locList;
+
+  private MapDrawController mapDrawer;
+
   @FXML
   void clearFields() {
     longNameTextField.clear();
@@ -52,8 +61,76 @@ public class MapEditorPageController {
     Navigation.navigate(Screen.HOME);
   }
 
+  private EventHandler<MouseEvent> paneFunction(Node node) {
+    return new EventHandler<MouseEvent>() {
+      @Override
+      public void handle(MouseEvent event) {
+        currentNodeEdit = node;
+        nodeInformationText.setText(currentNodeEdit.getNodeID());
+        longNameTextField.setText(currentNodeEdit.getLocation().getLongName());
+        shortNameTextField.setText(currentNodeEdit.getLocation().getShortName());
+        roomTypeTextField.setText(currentNodeEdit.getLocation().getLocationType());
+      }
+    };
+  }
+
   @FXML
-  void submit() {}
+  void submit() {
+    // For now, this just does basic changes. Will be edited when the changes required are more
+    // defined
+    Node newNode = new Node();
+    newNode.setLocation(
+        new locationName(
+            longNameTextField.getText(),
+            shortNameTextField.getText(),
+            roomTypeTextField.getText()));
+    newNode.setNodeID(currentNodeEdit.getNodeID());
+    newNode.setXcoord(currentNodeEdit.getXcoord());
+    newNode.setYcoord(currentNodeEdit.getYcoord());
+    newNode.setBuilding(currentNodeEdit.getBuilding());
+    newNode.setFloor(currentNodeEdit.getFloor());
+
+    for (Edge edge : currentNodeEdit.getNodeEdges()) {
+      for (Edge e : edge.getToNode().getNodeEdges()) {
+        if (e.getToNode().equals(currentNodeEdit)) {
+          edge.getToNode().getNodeEdges().remove(e);
+          e.setToNode(null);
+          e.setFromNode(null);
+        }
+      }
+      edge.setToNode(null);
+      edge.setFromNode(null);
+    }
+
+    newNode.setNodeEdges(new ArrayList<Edge>());
+
+    nodeList.remove(currentNodeEdit);
+    nodeList.add(newNode);
+
+    GesturePane gesturePane = (GesturePane) mapEditorPane.getCenter();
+    AnchorPane anchor = (AnchorPane) gesturePane.getContent();
+
+    for (javafx.scene.Node node : anchor.getChildren()) {
+      if ((currentNodeEdit.getNodeID() + "_pane").equals(node.getId())) {
+        anchor.getChildren().remove(node);
+        break;
+      }
+    }
+
+    // TODO should make a PaneFactory for this
+    final Pane tempPane = new Pane();
+    tempPane.setPrefSize(10, 10);
+    tempPane.setLayoutX(newNode.getXcoord() - 10 / 2);
+    tempPane.setLayoutY(newNode.getYcoord() - 10 / 2);
+    tempPane.setStyle("-fx-background-color: '#013A75';");
+    tempPane.setOnMouseClicked(paneFunction(newNode));
+    tempPane.setId(newNode.getNodeID() + "_pane");
+    anchor.getChildren().add(tempPane);
+
+    currentNodeEdit = newNode;
+
+    // TODO update database to match
+  }
 
   @FXML
   void displayHelp() {
@@ -65,20 +142,23 @@ public class MapEditorPageController {
 
   @FXML
   public void initialize() {
-    MapDrawController mapDrawer = new MapDrawController();
+    mapDrawer = new MapDrawController();
     Connection conn = makeConnection();
-    ArrayList<Node> nodeList = createJavaNodes(conn);
+
+    nodeList = createJavaNodes(conn);
+    locList = createJavaLocat(conn);
+
+    // TODO get node-location association from DB. This is temporary
+
+    for (Node node : nodeList) {
+      node.setLocation(new locationName("", "", ""));
+    }
+
     mapEditorPane.setCenter(
         mapDrawer.genMapFromNodes(
             nodeList,
             node -> {
-              return new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent event) {
-                  currentNodeEdit = node;
-                  nodeInformationText.setText(currentNodeEdit.getNodeID());
-                }
-              };
+              return paneFunction(node);
             }));
   }
 }
