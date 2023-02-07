77c42136c0ae2acefad5f6a3c57d525522df6be4
annreillyzimmerman <104455805+annreillyzimmerman@users.noreply.github.com>
2023-02-07 10:09:50 -0500

    added comments to MapEditorNodeController class

--- src/main/java/edu/wpi/cs3733/C23/teamD/controllers/pathfinding/MapEditorNodeController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/controllers/pathfinding/MapEditorNodeController.java
@@ -9,59 +9,62 @@ import org.controlsfx.control.PopOver;
 
 public class MapEditorNodeController {
 
-  Node node;
-  int y;
-  int x;
-  @FXML Pane mapEditorPane;
+  /* Attributes */
+  Node node; // contains pathfinding node information
 
   MapEditorNodeController(Node node) {
-    node = node;
-    x = node.getXcoord();
-    y = node.getYcoord();
     makeEditorNode();
   }
 
+
+  /**
+   * Creates a popover object that contains necessary popup information
+   * (pane, text containing node information).
+   * neither param nor return
+   */
   // TODO: make this happen during node hover, not node click
   private void makeEditorNode() {
-    PopOver popover = new PopOver();
-    Pane pane = new Pane();
+    PopOver popover = new PopOver(); // creates PopOver container
+    Pane pane = new Pane(); // creates Pane object to place within PopOver
+
+    // Start format PopOver container
     pane.setStyle("-fx-background-color: '#ffffff';");
     popover.setPrefHeight(100);
     popover.setPrefWidth(200);
     pane.setPrefHeight(100);
     pane.setPrefWidth(200);
     popover.setArrowSize(0);
+    // End format PopOver container
 
-    // REMOVE after database connection
-    if (node != null) {
+    if (node != null) { // connection to database is intact
       popover.setTitle(String.format("%s Information", node.getLongName()));
       Text nodeData =
-          new Text(
-              String.format(
-                  "\n  xCoord: %d \n  yCoord: %d \n  floor: %d \n  shortName: %s \n  longName: %s ",
-                  node.getXcoord(),
-                  node.getYcoord(),
-                  node.getFloor(),
-                  node.getShortName(),
-                  node.getLongName()));
+              new Text(
+                      String.format(
+                              "\n  xCoord: %d \n  yCoord: %d \n  floor: %s \n  shortName: %s \n  longName: %s ",
+                              node.getXcoord(),
+                              node.getYcoord(),
+                              node.getFloor(),
+                              node.getShortName(),
+                              node.getLongName()));
       nodeData.setStyle("-fx-font-family: OpenSans");
       pane.getChildren().add(nodeData);
-    } else {
+    } else { // connection to database is null (test case)
       popover.setTitle(String.format("%s Information", "nodeLongName"));
       Text nodeData =
-          new Text(
-              String.format(
-                  "\n  xCoord: %d \n  yCoord: %d \n  floor: %d \n  shortName: %s \n  longName: %s ",
-                  -10, -10, -10, "getShortName", "getLongName"));
+              new Text(
+                      String.format(
+                              "\n  xCoord: %d \n  yCoord: %d \n  floor: %d \n  shortName: %s \n  longName: %s ",
+                              -10, -10, -10, "getShortName", "getLongName"));
       nodeData.setStyle("-fx-font-family: OpenSans");
-      pane.getChildren().add(nodeData);
+      pane.getChildren().add(nodeData); // adds text to pane
     }
 
-    popover.setContentNode(pane);
-    popover.show(App.getPrimaryStage());
+    popover.setContentNode(pane); // sets pane as content of popover
+    popover.show(App.getPrimaryStage()); // shows popover on primary stage
   }
 
-  public void initialize() {}
+  public void initialize() {
+  }
+} // end class
 
-  public void testButtonFunction() {}
-}
