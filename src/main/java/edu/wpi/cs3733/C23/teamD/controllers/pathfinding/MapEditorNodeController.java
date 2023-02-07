584408eaafa22b813d51d3d0c05de03b5fd73780
annreillyzimmerman <104455805+annreillyzimmerman@users.noreply.github.com>
2023-02-07 09:44:11 -0500

    popover appears with node information.

--- src/main/java/edu/wpi/cs3733/C23/teamD/controllers/pathfinding/MapEditorNodeController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/controllers/pathfinding/MapEditorNodeController.java
@@ -2,10 +2,9 @@ package edu.wpi.cs3733.C23.teamD.controllers.pathfinding;
 
 import edu.wpi.cs3733.C23.teamD.App;
 import edu.wpi.cs3733.C23.teamD.entities.Node;
-import java.io.IOException;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import org.controlsfx.control.PopOver;
 
 public class MapEditorNodeController {
@@ -22,17 +21,43 @@ public class MapEditorNodeController {
     makeEditorNode();
   }
 
+  // TODO: make this happen during node hover, not node click
   private void makeEditorNode() {
     PopOver popover = new PopOver();
-    try {
-      System.out.println("it worked");
-      final FXMLLoader loader = new FXMLLoader(App.class.getResource("views/MapEditorPopup.fxml"));
-      popover.setContentNode(loader.load());
-      popover.setTitle("my node title");
-    } catch (IOException | NullPointerException e) {
-      e.printStackTrace();
+    Pane pane = new Pane();
+    pane.setStyle("-fx-background-color: '#ffffff';");
+    popover.setPrefHeight(200);
+    popover.setPrefWidth(200);
+    pane.setPrefHeight(200);
+    pane.setPrefWidth(200);
+    popover.setArrowLocation(PopOver.ArrowLocation.TOP_CENTER); // TODO: fix this so there is no arrow
+
+    // REMOVE after database connection
+    if (node != null) {
+      popover.setTitle(String.format("%s Information", node.getLongName()));
+      Text nodeData =
+          new Text(
+              String.format(
+                  "\n\n  xCoord: %d \n  yCoord: %d \n  floor: %d \n  shortName: %s \n  longName: %s ",
+                  node.getXcoord(),
+                  node.getYcoord(),
+                  node.getFloor(),
+                  node.getShortName(),
+                  node.getLongName()));
+      nodeData.setStyle("-fx-font-family: OpenSans");
+      pane.getChildren().add(nodeData);
+    } else {
+      popover.setTitle(String.format("%s Information", "nodeLongName"));
+      Text nodeData =
+          new Text(
+              String.format(
+                  "\n\n  xCoord: %d \n  yCoord: %d \n  floor: %d \n  shortName: %s \n  longName: %s ",
+                  -10, -10, -10, "getShortName", "getLongName"));
+      nodeData.setStyle("-fx-font-family: OpenSans");
+      pane.getChildren().add(nodeData);
     }
 
+    popover.setContentNode(pane);
     popover.show(App.getPrimaryStage());
   }
 
