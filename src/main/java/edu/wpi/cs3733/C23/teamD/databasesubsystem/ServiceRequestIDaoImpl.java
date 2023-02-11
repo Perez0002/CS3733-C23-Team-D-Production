721579f0deeed91b6e2f239a27157d0d562c469a
Ari Schechter <43043654+aschechter10@users.noreply.github.com>
2023-02-11 18:20:42 -0500

    finished ServiceRequest Dao impl just need to add wrappers in facade

--- src/main/java/edu/wpi/cs3733/C23/teamD/databasesubsystem/ServiceRequestIDaoImpl.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/databasesubsystem/ServiceRequestIDaoImpl.java
@@ -23,7 +23,13 @@ public class ServiceRequestIDaoImpl implements IDao<ServiceRequest> {
 
     @Override
     public ServiceRequest get(ServiceRequest s) {
-        return this.masterList.stream().filter(ServiceRequestForm -> s.getServiceRequestId() == (ServiceRequestForm.getServiceRequestId())).findFirst().orElse(null);
+        if (s instanceof PatientTransportRequest) {
+            return this.patientTransportRequestList.stream().filter(patientTransportRequest -> s.getServiceRequestId() == (patientTransportRequest.getServiceRequestId())).findFirst().orElse(null);
+        } else if (s instanceof SanitationRequest) {
+            return this.sanitationRequestList.stream().filter(sanitationRequest -> s.getServiceRequestId() == (sanitationRequest.getServiceRequestId())).findFirst().orElse(null);
+        } else {
+            return this.masterList.stream().filter(serviceRequest -> s.getServiceRequestId() == (serviceRequest.getServiceRequestId())).findFirst().orElse(null);
+        }
     }
 
     @Override
@@ -32,6 +38,12 @@ public class ServiceRequestIDaoImpl implements IDao<ServiceRequest> {
         try {
             session.persist(s);
             session.getTransaction().commit();
+
+            if (s instanceof PatientTransportRequest) {
+                this.patientTransportRequestList.add((PatientTransportRequest) s);
+            } else if (s instanceof SanitationRequest) {
+                this.sanitationRequestList.add((SanitationRequest) s);
+            }
 
             this.masterList.add(s);
 
@@ -47,12 +59,22 @@ public class ServiceRequestIDaoImpl implements IDao<ServiceRequest> {
             session.merge(s);
             session.getTransaction().commit();
 
-            int index = IntStream.range(0, this.masterList.size())
+            int index = this.findIndexOfObj(s);
+
+            if (s instanceof PatientTransportRequest) {
+                this.patientTransportRequestList.remove(index);
+                this.patientTransportRequestList.add((PatientTransportRequest) s);
+            } else if (s instanceof SanitationRequest) {
+                this.sanitationRequestList.remove(index);
+                this.sanitationRequestList.add((SanitationRequest) s);
+            }
+
+            int masterIndex = IntStream.range(0, this.masterList.size())
                     .filter(i -> this.masterList.get(i).getServiceRequestId() == (s.getServiceRequestId()))
                     .findFirst()
                     .orElse(-1);
 
-            this.masterList.remove(index);
+            this.masterList.remove(masterIndex);
             this.masterList.add(s);
 
         } catch (Exception ex) {
@@ -65,15 +87,33 @@ public class ServiceRequestIDaoImpl implements IDao<ServiceRequest> {
         return this.masterList;
     }
 
+    public ArrayList<PatientTransportRequest> getAllPatientTransportRequests() {
+        return this.patientTransportRequestList;
+    }
+
+    public ArrayList<SanitationRequest> getAllSanitationRequests() {
+        return this.sanitationRequestList;
+    }
+
     @Override
     public void refresh() {
-        ArrayList<ServiceRequest> javamasterList;
+        ArrayList<ServiceRequest> javaMasterList = new ArrayList<>();
+        ArrayList<PatientTransportRequest> javaPatientTransportRequestList;
+        ArrayList<SanitationRequest> javaSanitationRequestList;
+
         session.beginTransaction();
         try {
-            javamasterList =
-                    (ArrayList<ServiceRequest>) session.createQuery("SELECT s FROM ServiceRequest s", ServiceRequest.class).getResultList();
+            javaPatientTransportRequestList =
+                    (ArrayList<PatientTransportRequest>) session.createQuery("SELECT p FROM PatientTransportRequest p", PatientTransportRequest.class).getResultList();
+            javaSanitationRequestList =
+                    (ArrayList<SanitationRequest>) session.createQuery("SELECT p FROM SanitationRequest p", SanitationRequest.class).getResultList();
             session.getTransaction().commit();
-            this.masterList = javamasterList;
+
+            javaMasterList.addAll(javaPatientTransportRequestList);
+            javaMasterList.addAll(javaSanitationRequestList);
+
+            this.masterList = javaMasterList;
+
         } catch (Exception ex) {
             session.getTransaction().rollback();
         }
@@ -88,10 +128,33 @@ public class ServiceRequestIDaoImpl implements IDao<ServiceRequest> {
             int deleted = q.executeUpdate();
             session.getTransaction().commit();
 
+            if (s instanceof PatientTransportRequest) {
+                this.patientTransportRequestList.remove(s);
+            } else if (s instanceof SanitationRequest) {
+                this.sanitationRequestList.remove(s);
+            }
+
             this.masterList.remove(s);
 
         } catch (Exception ex) {
             session.getTransaction().rollback();
         }
+    }
+
+    private int findIndexOfObj(ServiceRequest s) {
+        int index = -1;
+        if (s instanceof PatientTransportRequest) {
+            index = IntStream.range(0, this.patientTransportRequestList.size())
+                    .filter(i -> this.patientTransportRequestList.get(i).getServiceRequestId() == (s.getServiceRequestId()))
+                    .findFirst()
+                    .orElse(-1);
+        } else if (s instanceof SanitationRequest) {
+            index = IntStream.range(0, this.sanitationRequestList.size())
+                    .filter(i -> this.sanitationRequestList.get(i).getServiceRequestId() == (s.getServiceRequestId()))
+                    .findFirst()
+                    .orElse(-1);
+        }
+
+        return index;
     }
 }
