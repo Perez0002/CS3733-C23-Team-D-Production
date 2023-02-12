4fa39c148a03fd5134b703bf1f387120754824c3
wyatt522 <75394799+wyatt522@users.noreply.github.com>
2023-02-12 13:52:58 -0500

    more styling + messing with the hompage controller now

--- src/main/java/edu/wpi/cs3733/C23/teamD/controllers/HomepageController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/controllers/HomepageController.java
@@ -1,7 +1,5 @@
 package edu.wpi.cs3733.C23.teamD.controllers;
 
-import edu.wpi.cs3733.C23.teamD.navigation.Navigation;
-import edu.wpi.cs3733.C23.teamD.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -38,12 +36,7 @@ public class HomepageController {
   @FXML private Label titleLabel;
 
   @FXML
-  public void initialize() {
-    serviceRequestFormButton.setOnMouseClicked(
-        event -> Navigation.navigate(Screen.SERVICE_REQUEST));
-    DBEditorButton.setOnMouseClicked(event -> Navigation.navigate(Screen.DATABASE_EDITOR));
-    mapEditorButton.setOnMouseClicked(event -> Navigation.navigate(Screen.MAP_EDITOR));
-  }
+  public void initialize() {}
 
   @FXML
   /** user open menubutton, clicks Exit, and it closes the window. */
@@ -57,13 +50,15 @@ public class HomepageController {
    * empty when button clicked again
    */
   void toggleHelpText(ActionEvent event) {
-    if (serviceRequestHelpText.getText().equals("") || bottomHelpText.getText().equals("")) {
-      serviceRequestHelpText.setText("Click the buttons below to fill out service request forms!");
-      bottomHelpText.setText(
-          "<-Use the leftmost button to exit the program  \n  Click the rightmost button to remove the help text->");
-    } else {
-      serviceRequestHelpText.setText("");
-      bottomHelpText.setText("");
-    }
+    //    if (serviceRequestHelpText.getText().equals("") || bottomHelpText.getText().equals("")) {
+    //      serviceRequestHelpText.setText("Click the buttons below to fill out service request
+    // forms!");
+    //      bottomHelpText.setText(
+    //          "<-Use the leftmost button to exit the program  \n  Click the rightmost button to
+    // remove the help text->");
+    //    } else {
+    //      serviceRequestHelpText.setText("");
+    //      bottomHelpText.setText("");
+    //    }
   }
 }
