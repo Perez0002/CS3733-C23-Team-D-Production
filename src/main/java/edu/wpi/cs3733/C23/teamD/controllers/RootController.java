d4c26c5febfce134f40060e2db6c7dbfa8ec673a
Liv <op90210@gmail.com>
2023-02-12 12:52:42 -0500

    added logo to top left corner

--- src/main/java/edu/wpi/cs3733/C23/teamD/controllers/RootController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/controllers/RootController.java
@@ -15,6 +15,7 @@ public class RootController {
 
   @FXML private MFXButton helpPageButton;
 
+  @FXML private MFXButton homeButton1;
   @FXML private MFXButton homeButton;
 
   @FXML private MFXButton infoButton;
@@ -27,28 +28,23 @@ public class RootController {
 
   @FXML
   public void initialize() {
-    homeButton.setOnMouseClicked(
-            event -> Navigation.navigate(Screen.HOME));
+    homeButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
     homeButton.setTooltip(new Tooltip("Home"));
 
-    profileButton.setOnMouseClicked(
-            event -> Navigation.navigate(Screen.HOME));
+    profileButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
     profileButton.setTooltip(new Tooltip("Profile Page"));
 
     serviceRequestFormsButton.setOnMouseClicked(
-            event -> Navigation.navigate(Screen.SERVICE_REQUEST));
+        event -> Navigation.navigate(Screen.SERVICE_REQUEST));
     serviceRequestFormsButton.setTooltip(new Tooltip("Service Request Forms"));
 
-    dbButton.setOnMouseClicked(
-            event -> Navigation.navigate(Screen.DATABASE_EDITOR));
+    dbButton.setOnMouseClicked(event -> Navigation.navigate(Screen.DATABASE_EDITOR));
     dbButton.setTooltip(new Tooltip("Database Editors"));
 
-    mapEditorButton.setOnMouseClicked(
-            event -> Navigation.navigate(Screen.MAP_EDITOR));
+    mapEditorButton.setOnMouseClicked(event -> Navigation.navigate(Screen.MAP_EDITOR));
     mapEditorButton.setTooltip(new Tooltip("Map Editor"));
 
-    helpPageButton.setOnMouseClicked(
-            event -> Navigation.navigate(Screen.HELP_PAGE));
+    helpPageButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HELP_PAGE));
     helpPageButton.setTooltip(new Tooltip("Help"));
 
     infoButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
