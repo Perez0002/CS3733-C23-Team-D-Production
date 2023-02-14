80cd34e7b0a39b72721dca5397e5ada39bed698e
Theo <tdwinter@wpi.edu>
2023-02-13 19:51:15 -0500

    Start ComputerServiceRequestController

--- src/main/java/edu/wpi/cs3733/C23/teamD/controllers/serviceRequestControllers/ComputerServiceRequestController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/controllers/serviceRequestControllers/ComputerServiceRequestController.java
@@ -1,4 +1,36 @@
 package edu.wpi.cs3733.C23.teamD.controllers.serviceRequestControllers;
 
-public class ComputerServiceRequestController {
+import edu.wpi.cs3733.C23.teamD.controllers.ServiceRequestController;
+import io.github.palexdev.materialfx.controls.MFXComboBox;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+import java.util.ArrayList;
+
+public class ComputerServiceRequestController extends ServiceRequestController {
+    @FXML
+    private ArrayList<String> urgency;
+    private ArrayList<String> deviceType;
+
+    @FXML
+    private MFXComboBox deviceTypeBox;
+    @FXML
+    private MFXComboBox urgencyBox;
+    @FXML
+    private MFXTextField descriptionBox;
+
+    public  ComputerServiceRequestController(){
+         urgency= new ArrayList<>();
+                urgency.add("High");
+                urgency.add("Medium");
+                urgency.add("Low");
+    }
+    public void initialize(){
+        urgencyBox.setItems(FXCollections.observableArrayList(urgency));
+
+
+
+    }
 }
