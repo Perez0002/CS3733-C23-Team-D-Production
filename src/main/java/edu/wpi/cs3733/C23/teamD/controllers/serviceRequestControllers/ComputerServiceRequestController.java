be5ec777f76d06c7b9d5773f3210eafff02aeaec
Theo <tdwinter@wpi.edu>
2023-02-13 20:49:32 -0500

    Computer Service Request done???

--- src/main/java/edu/wpi/cs3733/C23/teamD/controllers/serviceRequestControllers/ComputerServiceRequestController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/controllers/serviceRequestControllers/ComputerServiceRequestController.java
@@ -1,6 +1,9 @@
 package edu.wpi.cs3733.C23.teamD.controllers.serviceRequestControllers;
 
 import edu.wpi.cs3733.C23.teamD.controllers.ServiceRequestController;
+import edu.wpi.cs3733.C23.teamD.databasesubsystem.FDdb;
+import edu.wpi.cs3733.C23.teamD.entities.ComputerServiceRequest;
+import edu.wpi.cs3733.C23.teamD.entities.ServiceRequest;
 import io.github.palexdev.materialfx.controls.MFXComboBox;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.collections.FXCollections;
@@ -11,7 +14,6 @@ import java.util.ArrayList;
 
 public class ComputerServiceRequestController extends ServiceRequestController {
     @FXML
-    private ArrayList<String> urgency;
     private ArrayList<String> deviceType;
 
     @FXML
@@ -19,14 +21,29 @@ public class ComputerServiceRequestController extends ServiceRequestController {
     @FXML
     private MFXComboBox urgencyBox;
     @FXML
+    private MFXComboBox employeeBox;
+    @FXML
     private MFXTextField descriptionBox;
+    @FXML
+    private MFXComboBox locationBox;
+
 
     public  ComputerServiceRequestController(){
+        deviceType.add("Laptop Computer");
+        deviceType.add("Desktop Computer");
+        deviceType.add("Tablet");
+        deviceType.add("Kiosk");
+        deviceType.add("Other");
+    }
+
+    public void initialize(){
+        deviceTypeBox.setItems(FXCollections.observableArrayList(deviceType));)
+
+
 
     }
-    public void initialize(){
-
-
-
+    public void submit(){
+        ComputerServiceRequest computerServiceRequest= new ComputerServiceRequest(descriptionBox.getText(),employeeBox.getText(), ServiceRequest.Status.PROCESSING,urgencyBox.getText(),deviceTypeBox.getText(),locationBox.getText());
+        FDdb.getInstance().saveServiceRequest(computerServiceRequest);
     }
 }
--- src/main/java/edu/wpi/cs3733/C23/teamD/entities/ComputerServiceRequest.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/entities/ComputerServiceRequest.java
@@ -3,12 +3,20 @@ package edu.wpi.cs3733.C23.teamD.entities;
 import java.util.Date;
 
 public class ComputerServiceRequest extends ServiceRequest {
+  private String urgency;
+  private String deviceType;
+  private String location;
+
   public ComputerServiceRequest() {
     super();
   }
 
-  public ComputerServiceRequest(String reason, String staff, Status stat) {
+  public ComputerServiceRequest(String reason, String staff, Status stat,String urgency,String deviceType, String location) {
     super(staff, stat, reason, "ComputerService");
+    this.urgency=urgency;
+    this.deviceType=deviceType;
+    this.location=location;
+
   }
 
   public ComputerServiceRequest(
