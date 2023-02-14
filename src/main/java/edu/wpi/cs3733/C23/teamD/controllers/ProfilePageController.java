51d7f21decec75f7055156ec24739bb0a952dc0a
wyatt522 <75394799+wyatt522@users.noreply.github.com>
2023-02-13 23:16:26 -0500

    fixed name + initialized floating text template

--- src/main/java/edu/wpi/cs3733/C23/teamD/controllers/ProfilePageController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/controllers/ProfilePageController.java
@@ -2,57 +2,47 @@ package edu.wpi.cs3733.C23.teamD.controllers;
 
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXTextField;
-import jakarta.persistence.Table;
 import javafx.fxml.FXML;
-import javafx.scene.control.Tab;
-
-import javax.annotation.processing.Generated;
-import javax.swing.table.TableColumn;
-import javax.swing.text.TableView;
 
 public class ProfilePageController {
 
-    @FXML
-    private MFXButton changeProfilePicture;
+  @FXML private MFXButton changeProfilePicture;
 
-    @FXML
-    private MFXButton viewPassword;
+  @FXML private MFXButton viewPassword;
 
-    @FXML
-    private MFXButton changePassword;
+  @FXML private MFXButton changePassword;
 
-    @FXML
-    private MFXTextField email;
+  @FXML private MFXTextField email;
 
-    @FXML
-    private MFXTextField phoneNumber;
+  @FXML private MFXTextField phoneNumber;
 
-    @FXML
-    private MFXTextField address;
+  @FXML private MFXTextField address;
 
-    @FXML
-    private MFXTextField birthday;
+  @FXML private MFXTextField birthday;
 
-    @FXML
-    private MFXTextField accountCreated;
+  @FXML private MFXTextField accountCreated;
 
-    @FXML
-    private TableView serviceRequestHistory;
+  //  @FXML private TableView serviceRequestHistory;
+  //
+  //  @FXML private TableView databaseEditHistory;
+  //
+  //  @FXML private TableColumn serviceRequests;
+  //
+  //  @FXML private TableColumn serviceDates;
+  //
+  //  @FXML private TableColumn databaseEdits;
+  //
+  //  @FXML private TableColumn databaseEditDates;
 
-    @FXML
-    private TableView databaseEditHistory;
+  public void initialize() {
+    setFloatingText();
+  }
 
-    @FXML
-    private TableColumn serviceRequests;
-
-    @FXML
-    private TableColumn serviceDates;
-
-    @FXML
-    private TableColumn databaseEdits;
-
-    @FXML
-    private TableColumn databaseEditDates;
-
-
+  private void setFloatingText() {
+    accountCreated.setFloatingText("");
+    birthday.setFloatingText("");
+    email.setFloatingText("");
+    phoneNumber.setFloatingText("");
+    address.setFloatingText("");
+  }
 }
