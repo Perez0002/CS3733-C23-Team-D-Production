9dcaa99237e52e13dd0949d378f04bc2e63a03f0
Ari Schechter <43043654+aschechter10@users.noreply.github.com>
2023-02-14 20:47:13 -0500

    controller and entity mostly finished

--- src/main/java/edu/wpi/cs3733/C23/teamD/controllers/serviceRequestControllers/SecurityServiceRequestController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/controllers/serviceRequestControllers/SecurityServiceRequestController.java
@@ -6,58 +6,44 @@ import edu.wpi.cs3733.C23.teamD.controllers.components.RoomPickComboBoxControlle
 import edu.wpi.cs3733.C23.teamD.controllers.components.UrgencySelectorBoxController;
 import edu.wpi.cs3733.C23.teamD.databasesubsystem.FDdb;
 import edu.wpi.cs3733.C23.teamD.entities.SecurityServiceRequest;
-import edu.wpi.cs3733.C23.teamD.entities.ServiceRequest;
 import io.github.palexdev.materialfx.controls.MFXCircleToggleNode;
 import io.github.palexdev.materialfx.controls.MFXTextField;
-import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 
 public class SecurityServiceRequestController extends ServiceRequestController {
-   @FXML
-   private Parent staffDropdownBox;
-   @FXML
-   private EmployeeDropdownComboBoxController staffDropdownBoxController;
-   @FXML
-   private Parent locationDropdownBox;
+  @FXML private Parent staffDropdownBox;
+  @FXML private EmployeeDropdownComboBoxController staffDropdownBoxController;
+  @FXML private Parent locationDropdownBox;
 
-   @FXML
-   private RoomPickComboBoxController locationDropdownBoxController;
+  @FXML private RoomPickComboBoxController locationDropdownBoxController;
 
-   @FXML
-   private Parent urgencyDropdownBox;
-   @FXML
-   private UrgencySelectorBoxController urgencyDropdownBoxController;
-   @FXML
-   private MFXCircleToggleNode addSecurityNode;
-   @FXML
-   private MFXCircleToggleNode addRequestSecurityNode;
-   @FXML
-   private MFXTextField problemTextField;
+  @FXML private Parent urgencyDropdownBox;
+  @FXML private UrgencySelectorBoxController urgencyDropdownBoxController;
+  @FXML private MFXCircleToggleNode addSecurityNode;
+  @FXML private MFXCircleToggleNode addRequestSecurityNode;
+  @FXML private MFXTextField problemTextField;
 
-   public SecurityServiceRequestController()
-   {
+  public SecurityServiceRequestController() {}
 
-   }
+  public void initialize() {
+    // TODO temp
+  }
 
-   public void initialize() {
-     // TODO temp
-   }
+  public void submit() {
+    String typeOfRequest = "";
+    if (addSecurityNode.isFocused()) {
+      typeOfRequest = "Add Security";
+    } else if (addRequestSecurityNode.isFocused()) {
+      typeOfRequest = "Request Security";
+    }
 
-   public void submit()
-   {
-      String typeOfRequest = "";
-      if(addSecurityNode.isFocused())
-      {
-         typeOfRequest = "Add Security";
-      } else if(addRequestSecurityNode.isFocused())
-      {
-         typeOfRequest = "Request Security";
-      }
-
-      SecurityServiceRequest securityServiceRequest = new SecurityServiceRequest(
-        typeOfRequest, urgencyDropdownBoxController.getEmployeeName(), staffDropdownBoxController.getEmployeeName(), ServiceRequest.Status.PROCESSING, problemTextField.getText()
-      );
-      FDdb.getInstance().saveServiceRequest(securityServiceRequest);
-   }
+    SecurityServiceRequest securityServiceRequest =
+        new SecurityServiceRequest(
+            typeOfRequest,
+            urgencyDropdownBoxController.getEmployeeName(),
+            staffDropdownBoxController.getEmployeeName(),
+            problemTextField.getText());
+    FDdb.getInstance().saveServiceRequest(securityServiceRequest);
+  }
 }
--- src/main/java/edu/wpi/cs3733/C23/teamD/entities/SecurityServiceRequest.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/entities/SecurityServiceRequest.java
@@ -1,24 +1,24 @@
 package edu.wpi.cs3733.C23.teamD.entities;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 public class SecurityServiceRequest extends ServiceRequest {
-    @Getter
-    private String typeOfSecurityRequest;
+  @Getter private String typeOfSecurityRequest;
 
-    @Getter
-    private String urgency;
+  @Getter private String urgency;
 
-    public SecurityServiceRequest(String typeOfSecurityRequest, String urgency, String staff, Status stat, String reason) {
-        super(staff, stat, reason, "Security");
-        this.typeOfSecurityRequest = typeOfSecurityRequest;
-        this.urgency = urgency;
-    }
+  public SecurityServiceRequest(
+      String typeOfSecurityRequest, String urgency, String staff, String reason) {
+    super(staff, reason, "Security");
+    this.typeOfSecurityRequest = typeOfSecurityRequest;
+    this.urgency = urgency;
+  }
 
+  public SecurityServiceRequest() {
+    super();
+    this.typeOfSecurityRequest = "";
+    this.urgency = "";
+  }
 }
