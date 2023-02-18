fad772df0e32722cd5e2e00a929181eeb84ec7dc
Theo <tdwinter@wpi.edu>
2023-02-18 13:06:55 -0500

    Git reset my branch back to main and purged all my commits, so NFC should work from here

--- src/main/java/edu/wpi/cs3733/C23/teamD/cardreader/CommPort.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/cardreader/CommPort.java
@@ -1,2 +1,58 @@
-package edu.wpi.cs3733.C23.teamD.cardreader;public class CommPort {
+package edu.wpi.cs3733.C23.teamD.cardreader;
+
+import com.fazecast.jSerialComm.*;
+import java.nio.charset.StandardCharsets;
+
+public class CommPort {
+    private SerialPort portActive = SerialPort.getCommPort("");
+
+    public CommPort(String deviceName) {
+        SerialPort portList[] = SerialPort.getCommPorts();
+        String messageWrite = "Who are you?";
+        byte[] writeBuffer = messageWrite.getBytes();
+        for (SerialPort port : portList) {
+            try {
+                port.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 1500, 10);
+                port.setBaudRate(9600);
+                port.openPort();
+
+                Thread.sleep(1500);
+                System.out.println(
+                        "Pinging "
+                                + port.getSystemPortName()
+                                + " with "
+                                + port.writeBytes(writeBuffer, writeBuffer.length)
+                                + " bytes");
+                Thread.sleep(900);
+                byte[] readBuffer = new byte[port.bytesAvailable()];
+                System.out.println(port.readBytes(readBuffer, readBuffer.length) + " Bytes read");
+                String messageRead = new String(readBuffer, StandardCharsets.UTF_8);
+                System.out.println(messageRead);
+                System.out.println(deviceName);
+                if (messageRead.contains(deviceName)) {
+                    portActive = port;
+                } else {
+                    System.out.println("No device detected on " + port.getSystemPortName());
+                    port.closePort();
+                }
+            } catch (Exception e) {
+                System.out.println("No device detected on " + port.getSystemPortName());
+            }
+        }
+    }
+
+    public SerialPort getPort() {
+        return portActive;
+    }
+
+    public void closeSerial() {
+        portActive.closePort();
+    }
+
+    public String readSerial() {
+        byte[] readBuffer = new byte[portActive.bytesAvailable()];
+        System.out.println(portActive.readBytes(readBuffer, readBuffer.length) + " Bytes read");
+        String messageRead = new String(readBuffer, StandardCharsets.UTF_8);
+        return messageRead;
+    }
 }
--- src/main/java/edu/wpi/cs3733/C23/teamD/userinterface/controllers/LoginController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/userinterface/controllers/LoginController.java
@@ -44,6 +44,51 @@ public class LoginController {
   helper function for submit() function,
   ensures all necessary fields are filled before submission
   */
+  public void initialize() {
+    CommPort commPort = new CommPort("NFC Card Reader");
+    commPort
+            .getPort()
+            .addDataListener(
+                    new SerialPortDataListener() {
+                      @Override
+                      public int getListeningEvents() {
+                        return SerialPort.LISTENING_EVENT_DATA_AVAILABLE;
+                      }
+
+                      @Override
+                      public void serialEvent(SerialPortEvent event) {
+                        if (event.getEventType() == SerialPort.LISTENING_EVENT_DATA_AVAILABLE) {
+                          if (commPort.readSerial().contains("41 38 EE 20")) {
+                            commPort.closeSerial();
+
+                            LoginChecker loginInfo =
+                                    new LoginChecker("admin", "admin"); // creates PatientTransportData object
+
+                            if (loginInfo.setAccessLevel()) {
+
+                              incorrectUsernameOrPasswordText.setVisible(false);
+                              try {
+                                App.getRootPane()
+                                        .setLeft(
+                                                FXMLLoader.load(
+                                                        getClass()
+                                                                .getResource(
+                                                                        "/edu/wpi/cs3733/C23/teamD/views/NavBar.fxml")));
+                              } catch (IOException e) {
+                                throw new RuntimeException(e);
+                              }
+
+                              Navigation.navigate(Screen.HOME);
+                              App.getPrimaryStage().setMaximized(true);
+                            }
+                          } else {
+                            System.out.println("Invalid Card");
+                          }
+                        }
+                      }
+                    });
+  }
+
   private boolean checkFields() {
     if (checkPassword() && checkUsername()) {
       return true;
