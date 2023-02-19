2227e8e6162a4a1994c49793287f2f664a811822
Liv <op90210@gmail.com>
2023-02-19 16:38:10 -0500

    made login with enter work with new texttield

--- src/main/java/edu/wpi/cs3733/C23/teamD/userinterface/controllers/LoginController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/userinterface/controllers/LoginController.java
@@ -5,11 +5,14 @@ import edu.wpi.cs3733.C23.teamD.navigation.Navigation;
 import edu.wpi.cs3733.C23.teamD.navigation.Screen;
 import edu.wpi.cs3733.C23.teamD.servicerequest.util.LoginChecker;
 import java.io.IOException;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.text.Text;
 
 public class LoginController {
@@ -24,15 +27,17 @@ public class LoginController {
   @FXML private PasswordField password;
 
   @FXML
-  public void initialize() throws IOException {
+  public void initialize() {
     password.setOnKeyPressed(
-        event -> {
-          try {
-            submitLogin();
-          } catch (IOException e) {
-            throw new RuntimeException(e);
-          }
-        });
+            event -> {
+              if (event.getCode().equals(KeyCode.ENTER)) {
+                try {
+                  submitLogin();
+                } catch (IOException e) {
+                  throw new RuntimeException(e);
+                }
+              }
+            });
   }
 
   @FXML
