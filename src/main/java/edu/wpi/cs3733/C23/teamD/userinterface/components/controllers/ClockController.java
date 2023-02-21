659c3ba8a4c203787417c07d12a2b2d642e22160
wyatt522 <75394799+wyatt522@users.noreply.github.com>
2023-02-20 21:45:07 -0500

    added full clock functionality

--- src/main/java/edu/wpi/cs3733/C23/teamD/userinterface/components/controllers/ClockController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/userinterface/components/controllers/ClockController.java
@@ -1,8 +1,32 @@
 package edu.wpi.cs3733.C23.teamD.userinterface.components.controllers;
-import java.time.LocalTime;
+
+import java.awt.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
 
 public class ClockController {
-    LocalTime now = new LocalTime.now();
 
+  @FXML Label timer;
 
+  @FXML
+  public void initialize() {
+
+    Timeline clock =
+        new Timeline(
+            new KeyFrame(
+                Duration.ZERO,
+                e ->
+                    timer.setText(
+                        LocalDateTime.now()
+                            .format(DateTimeFormatter.ofPattern("EEE, LLL dd, hh:mm a")))),
+            new KeyFrame(Duration.seconds(1)));
+    clock.setCycleCount(Animation.INDEFINITE);
+    clock.play();
+  }
 }
