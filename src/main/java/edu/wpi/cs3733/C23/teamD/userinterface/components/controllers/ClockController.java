33592ad01558574295dede91d517bd30b00adf96
wyatt522 <75394799+wyatt522@users.noreply.github.com>
2023-02-20 20:59:34 -0500

    made clock component

--- src/main/java/edu/wpi/cs3733/C23/teamD/userinterface/components/controllers/ClockController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/userinterface/components/controllers/ClockController.java
@@ -1,2 +1,8 @@
-package edu.wpi.cs3733.C23.teamD.userinterface.components.controllers;public class ClockController {
+package edu.wpi.cs3733.C23.teamD.userinterface.components.controllers;
+import java.time.LocalTime;
+
+public class ClockController {
+    LocalTime now = new LocalTime.now();
+
+
 }
