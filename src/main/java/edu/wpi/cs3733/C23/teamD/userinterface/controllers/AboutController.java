7b70138b71e3a8167554af54066b27e69b0827cc
Gibson Phillips <gcaryp4@gmail.com>
2023-02-25 16:47:15 -0500

    Replaced images with the grouped image and started the cropping process.

--- src/main/java/edu/wpi/cs3733/C23/teamD/userinterface/controllers/AboutController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/userinterface/controllers/AboutController.java
@@ -1,3 +1,15 @@
 package edu.wpi.cs3733.C23.teamD.userinterface.controllers;
 
-public class AboutController {}
+import javafx.fxml.FXML;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.image.ImageView;
+
+public class AboutController {
+  @FXML ImageView imageOne;
+
+  @FXML
+  public void initialize() {
+    imageOne.setViewport(new Rectangle2D(0, 0, 720, 720));
+    imageOne.setFitHeight(150);
+  }
+}
--- src/main/java/edu/wpi/cs3733/C23/teamD/userinterface/controllers/CreditsController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/userinterface/controllers/CreditsController.java
@@ -1,5 +1,3 @@
 package edu.wpi.cs3733.C23.teamD.userinterface.controllers;
 
-public class CreditsController {
-
-}
+public class CreditsController {}
