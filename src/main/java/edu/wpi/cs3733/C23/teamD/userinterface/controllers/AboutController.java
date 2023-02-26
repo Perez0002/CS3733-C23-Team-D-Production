017667f19cbc58382b2ac47caf9f106e44516e13
Gibosn <gcphillips@wpi.edu>
2023-02-26 16:12:35 -0500

    replaced all of the images with the group image and created rectangle objects to crop them.

--- src/main/java/edu/wpi/cs3733/C23/teamD/userinterface/controllers/AboutController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/userinterface/controllers/AboutController.java
@@ -6,10 +6,66 @@ import javafx.scene.image.ImageView;
 
 public class AboutController {
   @FXML ImageView imageOne;
+  @FXML ImageView imageTwo;
+  @FXML ImageView imageThree;
+  @FXML ImageView imageFour;
+  @FXML ImageView imageFive;
+  @FXML ImageView imageSix;
+  @FXML ImageView imageSeven;
+  @FXML ImageView imageEight;
+  @FXML ImageView imageNine;
+  @FXML ImageView imageTen;
+  @FXML ImageView imageEleven;
+  @FXML ImageView imageTwelve;
+  @FXML ImageView imageThirteen;
+  @FXML ImageView imageFourteen;
+  @FXML ImageView imageFifteen;
 
   @FXML
   public void initialize() {
     imageOne.setViewport(new Rectangle2D(0, 0, 720, 720));
     imageOne.setFitHeight(150);
+
+    imageTwo.setViewport(new Rectangle2D(0, 0, 720, 720));
+    imageTwo.setFitHeight(150);
+
+    imageThree.setViewport(new Rectangle2D(0, 0, 720, 720));
+    imageThree.setFitHeight(150);
+
+    imageFour.setViewport(new Rectangle2D(0, 0, 720, 720));
+    imageFour.setFitHeight(150);
+
+    imageFive.setViewport(new Rectangle2D(0, 0, 720, 720));
+    imageFive.setFitHeight(150);
+
+    imageSix.setViewport(new Rectangle2D(0, 0, 720, 720));
+    imageSix.setFitHeight(150);
+
+    imageSeven.setViewport(new Rectangle2D(0, 0, 720, 720));
+    imageSeven.setFitHeight(150);
+
+    imageEight.setViewport(new Rectangle2D(0, 0, 720, 720));
+    imageEight.setFitHeight(150);
+
+    imageNine.setViewport(new Rectangle2D(0, 0, 720, 720));
+    imageNine.setFitHeight(150);
+
+    imageTen.setViewport(new Rectangle2D(0, 0, 720, 720));
+    imageTen.setFitHeight(150);
+
+    imageEleven.setViewport(new Rectangle2D(0, 0, 720, 720));
+    imageEleven.setFitHeight(150);
+
+    imageTwelve.setViewport(new Rectangle2D(0, 0, 720, 720));
+    imageTwelve.setFitHeight(150);
+
+    imageThirteen.setViewport(new Rectangle2D(0, 0, 720, 720));
+    imageThirteen.setFitHeight(150);
+
+    imageFourteen.setViewport(new Rectangle2D(0, 0, 720, 720));
+    imageFourteen.setFitHeight(150);
+
+    imageFifteen.setViewport(new Rectangle2D(0, 0, 720, 720));
+    imageFifteen.setFitHeight(150);
   }
 }
