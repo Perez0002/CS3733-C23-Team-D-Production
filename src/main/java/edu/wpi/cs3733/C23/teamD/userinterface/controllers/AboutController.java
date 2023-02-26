d774dd555722f4439709a7b22ab36c403b880810
Gibosn <gcphillips@wpi.edu>
2023-02-26 17:38:38 -0500

    Named and oriented images. (needs to be finished later) merged with dev.

--- src/main/java/edu/wpi/cs3733/C23/teamD/userinterface/controllers/AboutController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/userinterface/controllers/AboutController.java
@@ -23,49 +23,64 @@ public class AboutController {
 
   @FXML
   public void initialize() {
-    imageOne.setViewport(new Rectangle2D(2162, 722, 714, 714));
+    // Abby
+    imageOne.setViewport(new Rectangle2D(2162, 724, 714, 714));
     imageOne.setFitHeight(150);
 
-    imageTwo.setViewport(new Rectangle2D(722, 0, 720, 720));
+    // Mike
+    imageTwo.setViewport(new Rectangle2D(722, 4, 714, 714));
     imageTwo.setFitHeight(150);
 
-    imageThree.setViewport(new Rectangle2D(0, 0, 720, 720));
+    // Bryce
+    imageThree.setViewport(new Rectangle2D(722, 724, 714, 714));
     imageThree.setFitHeight(150);
 
-    imageFour.setViewport(new Rectangle2D(0, 0, 720, 720));
+    // Gibson
+    imageFour.setViewport(new Rectangle2D(2882, 724, 714, 714));
     imageFour.setFitHeight(150);
 
-    imageFive.setViewport(new Rectangle2D(0, 0, 720, 720));
+    // Jonathan
+    imageFive.setViewport(new Rectangle2D(1442, 724, 714, 714));
     imageFive.setFitHeight(150);
 
-    imageSix.setViewport(new Rectangle2D(0, 0, 720, 720));
+    // Wyatt
+    imageSix.setViewport(new Rectangle2D(0, 0, 714, 714));
     imageSix.setFitHeight(150);
 
-    imageSeven.setViewport(new Rectangle2D(0, 0, 720, 720));
+    // Annie
+    imageSeven.setViewport(new Rectangle2D(0, 0, 714, 714));
     imageSeven.setFitHeight(150);
 
-    imageEight.setViewport(new Rectangle2D(0, 0, 720, 720));
+    // Theo
+    imageEight.setViewport(new Rectangle2D(0, 0, 714, 714));
     imageEight.setFitHeight(150);
 
-    imageNine.setViewport(new Rectangle2D(0, 0, 720, 720));
+    // Liv
+    imageNine.setViewport(new Rectangle2D(0, 0, 714, 714));
     imageNine.setFitHeight(150);
 
-    imageTen.setViewport(new Rectangle2D(0, 0, 720, 720));
+    // Ari
+    imageTen.setViewport(new Rectangle2D(0, 0, 714, 720));
     imageTen.setFitHeight(150);
 
-    imageEleven.setViewport(new Rectangle2D(0, 0, 720, 720));
+    // Qui
+    imageEleven.setViewport(new Rectangle2D(0, 0, 714, 714));
     imageEleven.setFitHeight(150);
 
-    imageTwelve.setViewport(new Rectangle2D(0, 0, 720, 720));
+    // Wong
+    imageTwelve.setViewport(new Rectangle2D(0, 0, 714, 714));
     imageTwelve.setFitHeight(150);
 
-    imageThirteen.setViewport(new Rectangle2D(0, 0, 720, 720));
+    // Andrew
+    imageThirteen.setViewport(new Rectangle2D(0, 0, 714, 714));
     imageThirteen.setFitHeight(150);
 
-    imageFourteen.setViewport(new Rectangle2D(0, 0, 720, 720));
+    // Hospital
+    imageFourteen.setViewport(new Rectangle2D(0, 0, 714, 714));
     imageFourteen.setFitHeight(150);
 
-    imageFifteen.setViewport(new Rectangle2D(0, 0, 720, 720));
+    // CS Department
+    imageFifteen.setViewport(new Rectangle2D(0, 0, 714, 714));
     imageFifteen.setFitHeight(150);
   }
 }
