c28137be30ba52838491592b73a8b7459c2bfe20
Gibson Phillips <gcaryp4@gmail.com>
2023-02-27 22:26:09 -0500

    More formatting changes and starting to get the right text into the popups

--- src/main/java/edu/wpi/cs3733/C23/teamD/userinterface/controllers/CreditsController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/userinterface/controllers/CreditsController.java
@@ -1,21 +1,6 @@
 package edu.wpi.cs3733.C23.teamD.userinterface.controllers;
 
-import javafx.animation.PathTransition;
-import javafx.fxml.FXML;
-import javafx.scene.layout.VBox;
-import javafx.scene.shape.Line;
-import javafx.util.Duration;
-
 public class CreditsController {
-  @FXML private VBox vBox;
-  // private Duration duration;
-  public void initialize() {
-    Line pathLine = new Line();
 
-    PathTransition pathTransition = new PathTransition();
-    pathTransition.setNode(vBox);
-    pathTransition.setDuration(Duration.seconds(10));
-    pathTransition.setPath(pathLine);
-    pathTransition.play();
-  }
+  public void initialize() {}
 }
