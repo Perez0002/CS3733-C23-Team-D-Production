015a0a95efc142e663378063713282aaec7723ed
wyatt522 <75394799+wyatt522@users.noreply.github.com>
2023-02-28 16:47:21 -0500

    added service request header that changes as you go through requests

--- src/main/java/edu/wpi/cs3733/C23/teamD/userinterface/controllers/AutoGeneratePopupController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/userinterface/controllers/AutoGeneratePopupController.java
@@ -2,10 +2,10 @@ package edu.wpi.cs3733.C23.teamD.userinterface.controllers;
 
 import edu.wpi.cs3733.C23.teamD.database.entities.Move;
 import edu.wpi.cs3733.C23.teamD.servicerequest.controllers.*;
-
 import java.awt.*;
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import lombok.Setter;
 import org.kordamp.ikonli.javafx.FontIcon;
@@ -21,6 +21,8 @@ public class AutoGeneratePopupController {
   @FXML private FontIcon icon4;
   @FXML private FontIcon icon5;
 
+  @FXML private Label requestHeader;
+
   @Setter private Move move;
 
   @Setter private MoveRequestTableController moveRequestTableController;
@@ -31,6 +33,7 @@ public class AutoGeneratePopupController {
   public void initialize() {
     switchVBox(ServiceRequests.SANITATION_REQUEST);
     currentIcon = icon1;
+    requestHeader.setText("Sanitation Request");
   }
 
   public void fillFields() {
@@ -62,36 +65,29 @@ public class AutoGeneratePopupController {
     moveRequestTableController.closePopOver();
   }
 
-  private void turnCurrentCheck()
-  {
+  private void turnCurrentCheck() {
     currentIcon.setIconLiteral("fltral-checkmark-circle-24");
     increaseIcon();
   }
 
-  private void turnCurrentX()
-  {
+  private void turnCurrentX() {
     currentIcon.setIconLiteral("fltral-dismiss-circle-24");
     increaseIcon();
   }
 
-  private void increaseIcon()
-  {
-    if (currentIcon.equals(icon1))
-    {
+  private void increaseIcon() {
+    if (currentIcon.equals(icon1)) {
       currentIcon = icon2;
-    }
-    else if (currentIcon.equals(icon2))
-    {
+      requestHeader.setText("Computer Request");
+    } else if (currentIcon.equals(icon2)) {
       currentIcon = icon3;
-    }
-    else if (currentIcon.equals(icon3))
-    {
+      requestHeader.setText("A/V Request");
+    } else if (currentIcon.equals(icon3)) {
       currentIcon = icon4;
-    }
-    else if (currentIcon.equals(icon4))
-    {
+      requestHeader.setText("Security Request");
+    } else if (currentIcon.equals(icon4)) {
       currentIcon = icon5;
+      requestHeader.setText("Patient Transport Request");
     }
   }
-
 }
