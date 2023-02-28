ce594e2887c97ddb13358e418565b5233af0aed1
wyatt522 <75394799+wyatt522@users.noreply.github.com>
2023-02-27 21:50:47 -0500

    made files for the details popup on display move page

--- src/main/java/edu/wpi/cs3733/C23/teamD/userinterface/controllers/RequestDetailsPopupController.java
+++ src/main/java/edu/wpi/cs3733/C23/teamD/userinterface/controllers/RequestDetailsPopupController.java
@@ -1,11 +1,14 @@
 package edu.wpi.cs3733.C23.teamD.userinterface.controllers;
 
-import edu.wpi.cs3733.C23.teamD.servicerequest.controllers.*;
-import java.io.IOException;
+import edu.wpi.cs3733.C23.teamD.servicerequest.controllers.NavigationServiceRequests;
+import edu.wpi.cs3733.C23.teamD.servicerequest.controllers.ServiceRequestVBoxController;
+import edu.wpi.cs3733.C23.teamD.servicerequest.controllers.ServiceRequests;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
 
-public class AutoGeneratePopupController {
+import java.io.IOException;
+
+public class RequestDetailsPopupController {
 
   @FXML private Pane pane;
 
@@ -21,42 +24,5 @@ public class AutoGeneratePopupController {
 
   void switchVBox(ServiceRequests switchTo) {
     currentController = NavigationServiceRequests.navigate(switchTo, getPane());
-  }
-
-  @FXML
-  void resetChanges() {}
-
-  @FXML
-  void submitChanges() throws IOException {
-    //        boolean submission = false;
-    //
-    //        if (currentController instanceof HubBoxController) {
-    //        } else if (currentController instanceof PatientTransportVBoxController) {
-    //            submission = ((PatientTransportVBoxController) currentController).submit();
-    //        } else if (currentController instanceof ComputerServiceRequestController) {
-    //            submission = ((ComputerServiceRequestController) currentController).submit();
-    //        } else if (currentController instanceof SecurityServiceRequestController) {
-    //            submission = ((SecurityServiceRequestController) currentController).submit();
-    //        } else if (currentController instanceof SanitationRequestController) {
-    //            submission = ((SanitationRequestController) currentController).submit();
-    //        } else if (currentController instanceof AVRequestController) {
-    //            submission = ((AVRequestController) currentController).submit();
-    //        } else {
-    //            currentController.submit();
-    //        }
-    //
-    //        if (submission) {
-    //            clearFields();
-    //            ToastController.makeText("Your form has been submitted!", 1500, 50, 100, 225,
-    // 740);
-    //            if (CurrentUserEnum._CURRENTUSER.getSetting().getConfetti() == 1) {
-    //                ConfettiController.makeConfetti(1500, 50, 100);
-    //            }
-    //            requiredFieldsText.setText("* Required fields.");
-    //            requiredFieldsText.setTextFill(Color.rgb(1, 45, 90));
-    //        } else {
-    //            requiredFieldsText.setText("* Please fill out the required fields.");
-    //            requiredFieldsText.setTextFill(Color.color(1, 0, 0));
-    //        }
   }
 }
