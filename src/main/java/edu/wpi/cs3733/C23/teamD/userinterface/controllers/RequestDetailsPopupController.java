d20ee4289c1eb6f6ab05cf88ded616bb5722756b
wyatt522 <75394799+wyatt522@users.noreply.github.com>
2023-02-27 21:50:07 -0500

    made files for the details popup on display move page

---
+++ src/main/java/edu/wpi/cs3733/C23/teamD/userinterface/controllers/RequestDetailsPopupController.java
@@ -0,0 +1,62 @@
+package edu.wpi.cs3733.C23.teamD.userinterface.controllers;
+
+import edu.wpi.cs3733.C23.teamD.servicerequest.controllers.*;
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
+public class AutoGeneratePopupController {
+
+  @FXML private Pane pane;
+
+  private ServiceRequestVBoxController currentController;
+
+  public void initialize() {
+    switchVBox(ServiceRequests.SANITATION_REQUEST);
+  }
+
+  public Pane getPane() {
+    return pane;
+  }
+
+  void switchVBox(ServiceRequests switchTo) {
+    currentController = NavigationServiceRequests.navigate(switchTo, getPane());
+  }
+
+  @FXML
+  void resetChanges() {}
+
+  @FXML
+  void submitChanges() throws IOException {
+    //        boolean submission = false;
+    //
+    //        if (currentController instanceof HubBoxController) {
+    //        } else if (currentController instanceof PatientTransportVBoxController) {
+    //            submission = ((PatientTransportVBoxController) currentController).submit();
+    //        } else if (currentController instanceof ComputerServiceRequestController) {
+    //            submission = ((ComputerServiceRequestController) currentController).submit();
+    //        } else if (currentController instanceof SecurityServiceRequestController) {
+    //            submission = ((SecurityServiceRequestController) currentController).submit();
+    //        } else if (currentController instanceof SanitationRequestController) {
+    //            submission = ((SanitationRequestController) currentController).submit();
+    //        } else if (currentController instanceof AVRequestController) {
+    //            submission = ((AVRequestController) currentController).submit();
+    //        } else {
+    //            currentController.submit();
+    //        }
+    //
+    //        if (submission) {
+    //            clearFields();
+    //            ToastController.makeText("Your form has been submitted!", 1500, 50, 100, 225,
+    // 740);
+    //            if (CurrentUserEnum._CURRENTUSER.getSetting().getConfetti() == 1) {
+    //                ConfettiController.makeConfetti(1500, 50, 100);
+    //            }
+    //            requiredFieldsText.setText("* Required fields.");
+    //            requiredFieldsText.setTextFill(Color.rgb(1, 45, 90));
+    //        } else {
+    //            requiredFieldsText.setText("* Please fill out the required fields.");
+    //            requiredFieldsText.setTextFill(Color.color(1, 0, 0));
+    //        }
+  }
+}
